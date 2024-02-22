2897916c788551a0da49186251028365a02e71eb
dirtandgrass <verdant.luke@gmail.com>
2024-02-22 10:06:14 -0500

    internalize listFiles, add isFile

--- server/services/Files.js
+++ server/services/Files.js
@@ -7,13 +7,32 @@ class Files {
     this.#path = path;
   }
 
+  async isFile(file) {
+    const promise = new Promise((resolve) => {
+      fs.stat(this.#path + file, (err, stats) => {
+        if (err) {
+          resolve(false);
+        } else {
+          resolve(stats.isFile());
+        }
+      });
+    });
+
+    return promise;
+  }
+
   async listFiles() {
     const promise = new Promise((resolve, reject) => {
       fs.readdir(this.#path, async(err, files) => {
         if (err) {
           reject(err);
         } else {
-          const filesOnly = await getFilesOnly(this.#path, files);
+          let filesOnly = [];
+          for (let file of files) {
+            if (await this.isFile(file)) {
+              filesOnly.push(file);
+            }
+          }
           resolve(filesOnly);
         }
       });
@@ -22,16 +41,6 @@ class Files {
   }
 }
 
-const getFilesOnly = (path, files) => {
-  const promise = new Promise((resolve) => {
 
-    const filesOnly = files.filter((file) => {
-      const stats = fs.statSync(path + file);
-      return stats.isFile();
-    });
-    resolve(filesOnly);
-  });
-  return promise;
-};
 
 module.exports = Files;
